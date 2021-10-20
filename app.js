4f01851cbf7524a9467c14dcf86cd38f6dcbaefe
Giovanni Noceti <giovanni.noceti@symbiose.cl>
2021-10-20 00:47:15 -0300

    agregados response y try catch, restan validaciones

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ mongoose
 	})
 	.catch((err) => console.log(err));
 
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	next();
+});
 app.use(express.json());
 app.use(router);
 app.use("/", express.static("public"));
--- routes/router.js
+++ routes/router.js
@@ -20,28 +20,29 @@ const vonage = new Vonage(
 	{ debug: true }
 );
 
-/* router.get("/hello", (req, res) => {
-	res.status(200).send("hello world");
-	console.log("hello world");
-}); */
 router.post("/sendSmsNotifications", (req, res) => {
-	const from = "Vonage APIs";
-	const to = req.body.number;
-	const text = req.body.text;
+	try {
+		const from = "Vonage APIs";
+		const to = req.body.number;
+		const text = req.body.text;
 
-	vonage.message.sendSms(from, to, text, (err, responseData) => {
-		if (err) {
-			console.log(err);
-		} else {
-			if (responseData.messages[0]["status"] === "0") {
-				console.log("Message sent successfully.");
+		vonage.message.sendSms(from, to, text, (err, responseData) => {
+			if (err) {
+				console.log(err);
 			} else {
-				console.log(
-					`Message failed with error: ${responseData.messages[0]["error-text"]}`
-				);
+				if (responseData.messages[0]["status"] === "0") {
+					console.log("Message sent successfully.");
+				} else {
+					console.log(
+						`Message failed with error: ${responseData.messages[0]["error-text"]}`
+					);
+				}
 			}
-		}
-	});
+			res.send(responseData);
+		});
+	} catch (error) {
+		res.send(error);
+	}
 });
 
 router.post("/sendMailNotifications", (req, res) => {
@@ -56,12 +57,13 @@ router.post("/sendMailNotifications", (req, res) => {
 		transporter.sendMail(mailOptions, (err, res) => {
 			if (err) {
 				console.log(err);
-				res.send(err);
 			} else {
 				console.log("Email sent" + res.response);
 			}
 		});
+		res.send(res);
 	} catch (err) {
+		res.send(err);
 		console.error(err);
 	}
 });
@@ -70,8 +72,10 @@ router.get("/suscribe", async (req, res) => {
 	try {
 		const suscribers = await Suscriber.find();
 		console.log(suscribers);
+		res.send("Se a suscrito con exito");
 	} catch (error) {
 		console.log(error);
+		res.send("A ocurrido un error");
 	}
 });
 router.post("/suscribe", async (req, res) => {
@@ -79,12 +83,14 @@ router.post("/suscribe", async (req, res) => {
 	try {
 		await Suscriber.create(body);
 		console.log(res);
+		res.send("Se a suscrito con exito");
 	} catch (error) {
 		console.log(error);
+		res.send("A ocurrido un error");
 	}
 });
 
-router.post("/contactUs", (req, res) => {
+router.post("/contactUs", async (req, res) => {
 	try {
 		console.log(req.body);
 		const mailOptions = {
@@ -93,16 +99,17 @@ router.post("/contactUs", (req, res) => {
 			subject: req.body.subject,
 			text: req.body.message,
 		};
-		transporter.sendMail(mailOptions, (err, res) => {
+		await transporter.sendMail(mailOptions, (err, res) => {
 			if (err) {
 				console.log(err);
-				res.send(err);
 			} else {
-				console.log("Email sent" + res.response);
+				console.log("Email sent");
 			}
 		});
+		res.sendStatus(200);
 	} catch (err) {
 		console.error(err);
+		res.send("A ocurrido un error mientras se enviaba en correo");
 	}
 });
 
